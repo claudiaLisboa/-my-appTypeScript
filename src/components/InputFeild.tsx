0c1e3145e3168badc5dd8f192f36c3fca16259dd
Claudia Lisboa <ccalumby@gmail.com>
2022-12-27 15:59:18 -0500

    created input components

---
+++ src/components/InputFeild.tsx
@@ -0,0 +1,14 @@
+//rafce  - ES7 React/Redux/GraphQl/React-Native (extension)
+import React from 'react'
+
+const InputFeild = () => {
+  return (
+    <form className='input'>
+        <input  />
+       
+       
+    </form>
+  )
+}
+
+export default InputFeild
