7827e770cdcee375f1bf442a21871bfaa7cb3e2d
Claudia Lisboa <ccalumby@gmail.com>
2022-12-27 16:43:36 -0500

    added style css

--- src/components/InputFeild.tsx
+++ src/components/InputFeild.tsx
@@ -1,11 +1,14 @@
 //rafce  - ES7 React/Redux/GraphQl/React-Native (extension)
-import React from 'react'
+import React from 'react';
+import './style.css';
 
 const InputFeild = () => {
   return (
     <form className='input'>
-        <input  />
-       
+        <input type="input" placeholder="Enter a task"  className="input__box"/>
+       <button className="input__submit" type="submit">
+        Go
+       </button>
        
     </form>
   )
