6c070b9e198da7a64a53c3d9cc2613a593aaea94
Claudia Lisboa <ccalumby@gmail.com>
2023-01-03 12:24:19 -0500

    added property onDragEnd

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const  App: React.FC = () => {
   };
 
   return (
-   <DragDropContext>
+   <DragDropContext onDragEnd={() => {}}>
       <div className="App"> 
             <span className="heading">Taskify</span>
             <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 //rafce  - ES7 React/Redux/GraphQl/React-Native (extension)
 import React from 'react';
+import { Droppable } from 'react-beautiful-dnd';
 import { Todo } from '../model';
 import SingleTodo from './SingleTodo';
 import './styles.css';
@@ -8,27 +9,30 @@ interface Props {
     todos:Todo[];
     setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
     completedTodos: Todo[];
-    setcompletedTodos:  React.Dispatch<React.SetStateAction<Todo[]>>;
+    setcompletedTodos :  React.Dispatch<React.SetStateAction<Todo[]>>;
 
 }
 
 const TodoList: React.FC<Props> = ({ todos, setTodos}: Props) => {
     return(
         <div className='container'>
-            <div className="todos">
-                <span className="todos__heading">
-                    Active Tasks
-                </span>
-                {
-                    todos.map(todo => ( 
-                      <SingleTodo 
-                            todo= {todo} key={todo.id}
-                            todos = {todos}
-                            setTodos ={setTodos}
-                     />
-                     ))
-                }
-            </div>
+            <Droppable>
+                <div className="todos">
+                    <span className="todos__heading">
+                        Active Tasks
+                    </span>
+                    {
+                        todos.map(todo => ( 
+                        <SingleTodo 
+                                todo= {todo} key={todo.id}
+                                todos = {todos}
+                                setTodos ={setTodos}
+                        />
+                        ))
+                    }
+                </div>
+            </Droppable>
+           
             <div className="todos remove">
                 <span className="todos__heading">
                         Completed Tasks
