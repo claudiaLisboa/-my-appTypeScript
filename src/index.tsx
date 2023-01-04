0649fad52ca9c7dfa59197133ee23b93a613700f
Claudia Lisboa <ccalumby@gmail.com>
2023-01-03 22:01:17 -0500

    fixed error - failled droppable

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,18 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+ 
     <App />
-  </React.StrictMode>
+ 
 );
+
+// ERROR : Invariant failed: Cannot find droppable entry with id [TodosList]
+// https://github.com/atlassian/react-beautiful-dnd/issues/2396
+// Remove <React.StrictMode> </React.StrictMode> tag from Index.tsx file 
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 
