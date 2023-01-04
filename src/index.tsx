28fb38d65b9b2e51f8f47cc34a646eb4748aeb0c
Claudia Lisboa <ccalumby@gmail.com>
2023-01-03 22:04:32 -0500

    React 18’s “Strict mode”- react-beautiful-dnd

--- src/index.tsx
+++ src/index.tsx
@@ -16,6 +16,7 @@ root.render(
 
 // ERROR : Invariant failed: Cannot find droppable entry with id [TodosList]
 // https://github.com/atlassian/react-beautiful-dnd/issues/2396
+// https://medium.com/@wbern/getting-react-18s-strict-mode-to-work-with-react-beautiful-dnd-47bc909348e4
 // Remove <React.StrictMode> </React.StrictMode> tag from Index.tsx file 
 // root.render(
 //   <React.StrictMode>
